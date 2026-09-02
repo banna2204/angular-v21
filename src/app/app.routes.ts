58da74a727a76a784579392453e60dc7d404ab02
banna2204 <shubham.rajput2204@gmail.com>
2026-09-02 19:27:36 +0530

    create figma 2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { App } from './app';
 import { Figma } from './components/figma/figma';
+import { Figma2 } from './components/figma2/figma2';
 
 export const routes: Routes = [
     {path: '', component: App},
-    {path: 'figma', component: Figma}
+    {path: 'figma', component: Figma},
+    {path: 'figma2',component: Figma2}
 ];
---
+++ src/app/components/figma2/figma2.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-figma2',
+  imports: [MatIconModule],
+  templateUrl: './figma2.html',
+  styleUrl: './figma2.scss',
+})
+export class Figma2 {}
